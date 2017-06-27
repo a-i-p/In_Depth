7e5cee263f37018e064be265a7d29c6f78668ba8
a-i-p <anton.parfenov@gmail.com>
2017-06-27 17:32:16 +0300

    C# 1. Если get публичный, то и set обязан быть публичным. В ArrayList можно запихнуть что угодно.

---
+++ Chapter1/Product.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+
+namespace Chapter1
+{
+    public class Product
+    {
+        private string name;
+        private decimal price;
+
+        public Product(string name, decimal price)
+        {
+            Name = name;
+            Price = price;
+        }
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public decimal Price
+        {
+            get { return price; }
+            set { price = value; }
+        }
+
+        public static ArrayList GetSampleProducts()
+        {
+            ArrayList list = new ArrayList();
+            list.Add(new Product("West Side Story", 9.99m));
+            list.Add(new Product("Assassins", 14.99m));
+            list.Add(new Product("Frogs", 13.99m));
+            list.Add(new Product("Sweeney Todd", 10.99m));
+            return list;
+        }
+
+        public override string ToString()
+        {
+            return string.Format("{0}: {1}", name, price);
+        }
+    }
+}
